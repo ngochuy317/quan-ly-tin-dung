bac0a15244c9631cc75aa9a0c37f4654076d1245
Huy Nguyen <ngochuy317@gmail.com>
2023-05-17 22:40:16 +0700

    disable LoginRequiredMiddleware

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'apps.base.middleware.LoginRequiredMiddleware',
+    # 'apps.base.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
