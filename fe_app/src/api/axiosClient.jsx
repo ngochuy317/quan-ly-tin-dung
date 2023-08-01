4f4bce7e8bf0453bfc4b83b86492772488175e92
Huy Nguyen <ngochuy317@gmail.com>
2023-08-01 23:43:05 +0700

    new ci/cd flow

--- fe_app/src/api/axiosClient.jsx
+++ fe_app/src/api/axiosClient.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "http://165.22.102.193:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
